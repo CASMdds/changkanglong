1895328304da6bce3c601483aca7902d90d79cc3
CASMdds <98316350+CASMdds@users.noreply.github.com>
2022-01-26 16:37:28 +0800

    Update 测试文件.m

--- 测试文件.m
+++ 测试文件.m
@@ -9,3 +9,13 @@ f=7;
 fs=15;
 N=200;
 t=(0:N-1)/fs;
+y=sin(2*pi*f*t);
+Y=y+y0;%添加抖动偏移序列后的y方向
+%yh = hilbert(Y);
+%tfr = tfrstft(yh(:), 1:length(yh), 1024);
+
+
+% 连续小波变换
+wavename='cmor3-3';
+totalscal=256;
+Fc=centfrq(wavename); % 小波的中心频率
